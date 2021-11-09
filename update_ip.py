cff6dd5170770aafdca228267f1c96b20b9ced2e
Odracir <ricardogb92@gmail.com>
2021-11-09 11:28:29 -0700

    Update update_ip.py

--- update_ip.py
+++ update_ip.py
@@ -4,7 +4,7 @@ import certifi
 from xml.etree import ElementTree
 
 domain = "moldavian.xyz"
-APIKEY="25153307711d47d88147573e6c66f87a"  # Namecheap DDNS Token (Accounts > Domain List > Advanced DNS)
+APIKEY="25153307711d47d88227573e6c66f87a"  # Namecheap DDNS Token (Accounts > Domain List > Advanced DNS)
 
 def getIP():
 	r = requests.get("https://ifconfig.co/json", verify=certifi.where()).json()
